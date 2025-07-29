fdb43721f46851a26212abfb05651d02144dac81
szzdmj <chuanfuyin001@gmail.com>
2025-07-29 22:07:29 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+// 只转发请求到容器，不走 Durable Object
 export default {
   async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
     return env.container.fetch(request);
   },
 };
-
